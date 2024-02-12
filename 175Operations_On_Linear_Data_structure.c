814b93d119160510a46cd2e0318876bd7b19395c
CompileArtisan <praanesh.b.nair@gmail.com>
2024-02-12 23:26:01 +0530

    dsa

--- 175Operations_On_Linear_Data_structure.c
+++ 175Operations_On_Linear_Data_structure.c
@@ -1,11 +1,21 @@
 #include<stdio.h>
 
 // Transversal
-void transverse(int a[50] , int numberOfElements){
+void traversal(int a[50] , int numberOfElements){
     for(int i=0 ; i<numberOfElements ; i++) printf("%d ",a[i]);
 }
 
-
+// bubblesort
+void bubblesort(int a[50], int n){
+    for(int j=0 ; j<n-1 ; j++){
+        for(int i=0 ; i<n-1-j ; i++)
+            if(a[i]>=a[i+1]){
+                int temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;    
+            }        
+    }
+}
 
 // Search
 int search(int a[50] , int numberOfElements , int k){
@@ -19,8 +29,8 @@ int search(int a[50] , int numberOfElements , int k){
 
 // Inserting in Unsorted Array
 void insertInUnsorted(int a[50] , int n , int value , int index){
-    for(int i=n ; i>index ; i--)
-        a[i] = a[i-1];
+    for(int i=n-1 ; i>=index ; i--)
+        a[i+1] = a[i];
     a[index]=value;
 }
 
@@ -28,8 +38,8 @@ void insertInUnsorted(int a[50] , int n , int value , int index){
 // Inserting in Sorted Array
 void insertInSorted(int a[50] , int n , int value){
     int i=n-1;
-    for(; value<a[i] ; i--)
-        a[i] = a[i-1];
+    for(; a[i]>value ; i--)
+        a[i+1] = a[i];
     a[i+1]=value;
 }
 
@@ -39,41 +49,35 @@ void delete(int a[50] , int n , int index){
         a[i]=a[i+1];
 }
 
-// bubblesort
-void bubblesort(int a[50], int n){
-    for(int j=0 ; j<n ; j++){
-        for(int i=0 ; i<n-1 ; i++)
-            if(a[i]>=a[i+1]){
-                int temp=a[i];
-                a[i]=a[i+1];
-                a[i+1]=temp;
-            }        
-    }
-}
+
+
+// selectionsort
+
 
 
 
 
 int main(){
-    int a[50]={43,353,200,48};
-    transverse(a,4);
-    printf("\n\n%d\n\n",search(a,4,48));
-    insertInUnsorted(a,4,2,2);
-    transverse(a,5);
+    // int a[50]={1,2,3,4,5,393,8,9,10};
+    // insertInUnsorted(a,10,11,8);
+    // traversal(a,10);
 
-    printf("\n\n");
-    int b[50]={10,20,30,40,50,60};
-    insertInSorted(b,6,46);
-    transverse(b,7);
 
-    printf("\n\n");
-    delete(a,5,3);
-    transverse(a,4);
+    // int a[50]={10,20,30,40,50,60,70,80,90,100};
+    // insertInSorted(a,10,35);
+    // traversal(a,11);
 
-    printf("\n\n");
-    int c[50]={49,90,12,45};
-    bubblesort(c,4);
-    transverse(c,4);
+    // int a[50]={1,2,3,4,5};
+    // insertInUnsorted(a,10,55,3);
+    // traversal(a,6);
+
+
+    // int a[50]={1,2,3,4,5};
+    // delete(a,5,3);
+    // traversal(a,4);
+
+    
+
 
     return 0;
 }
